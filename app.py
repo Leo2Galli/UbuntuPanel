72a2c2e23da8bf053c9b5de6e673cf7621102957
Leo2Galli <167233052+Leo2Galli@users.noreply.github.com>
2024-09-22 21:11:54 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import os
 
 app = Flask(__name__)
 
-# Carica la porta dalla variabile d'ambiente
+# Porta configurabile dall'utente durante l'installazione
 port = int(os.getenv("PANEL_PORT", 5000))
 
+# Route per la homepage
 @app.route('/')
 def home():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=port)
+# Route per la console
+@app.route('/console')
+def console():
+    return render_template('console.html')
+
+# Route per i grafici
+@app.route('/graphs')
+def graphs():
+    return render_template('graphs.html')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=port, debug=True)
