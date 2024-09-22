00be243553ba515e726cc131113e16183b1cb950
Leo2Galli <167233052+Leo2Galli@users.noreply.github.com>
2024-09-22 16:00:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify({"message": "Benvenuto nel Pannello di Gestione Hosting!"})
+    return render_template('index.html')
 
-if __name__ == '__main__':
+@app.route('/console')
+def console():
+    return render_template('console.html')
+
+@app.route('/vps')
+def vps():
+    return render_template('vps.html')
+
+@app.route('/file_manager')
+def file_manager():
+    return render_template('file_manager.html')
+
+@app.route('/other_page')
+def other_page():
+    return render_template('other_page.html')
+
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
