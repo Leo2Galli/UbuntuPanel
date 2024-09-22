b9002ab2226c897a24d651f4f13bb737535983c3
Leo2Galli <167233052+Leo2Galli@users.noreply.github.com>
2024-09-22 17:02:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 from flask import Flask, render_template
 import os
+import socket
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    ip_address = socket.gethostbyname(socket.gethostname())
+    return render_template('index.html', ip_address=ip_address)
 
-@app.route('/stats')
-def stats():
-    return render_template('stats.html')
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+if __name__ == "__main__":
+    # Leggi la porta da una variabile d'ambiente
+    port = int(os.environ.get('PANEL_PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
