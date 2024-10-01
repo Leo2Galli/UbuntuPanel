6b47507284551e01ebdaed6f69596c89bb7dee5f
Leo2Galli <167233052+Leo2Galli@users.noreply.github.com>
2024-10-01 15:39:56 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+# Configurazioni del pannello
+
+# Porta in cui il pannello è in esecuzione
+PORT = 5000  # Sostituisci con il valore fornito durante l'installazione
+
+# Nome del pannello
+PANEL_NAME = 'UbuntuPanel'  # Sostituisci con il nome fornito durante l'installazione
